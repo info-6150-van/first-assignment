694c3e884eb1db6cba53d509ae9d0556689b633f
Nahom Araya <nahomaraya8@gmail.com>
2026-03-03 16:51:34 -0800

    activity tracker boilerplate

--- activity-tracker.js
+++ activity-tracker.js
@@ -1,5 +1,36 @@
+const STORAGE_KEY = 'activity-tracker-data';
+const MAX_EVENTS = 100;
+const MAX_TIMELINE_DISPLAY = 20;
+
 class ActivityTracker {
     // IMPLEMENT YOUR CODE HERE
+    constructor(options = {}) {
+        this.storage_key;
+        this.max_events;
+        this.data;
+        this.widgetElements;
+    }
+    generateSessionId() {}
+    loadOrCreateSession() {}
+    persist() {}
+    updateSessionDuration() {}
+    recordEvent(type, details) {}
+
+    getPageLabel(){}
+    formatDuration(ms) {}
+    formatTimestamp(ts) {}
+
+    renderWidget() {}
+    refreshWidget() {}
+    createEventItemElement() {}
+
+    attachEventListeners() {}
+    isWidgetElement(target) {}     
+    getClickTargetLabel(target) {}
+    handleDocumentClick(e) {}
+    handleDocumentSubmit(e) {}
+    handleToggleTimeline() {}
+  
 }
 
 // Export the class
@@ -8,3 +39,5 @@ if (typeof module !== 'undefined' && module.exports) {
 } else {
     window.ActivityTracker = ActivityTracker;
 }
+
+export default ActivityTracker;
